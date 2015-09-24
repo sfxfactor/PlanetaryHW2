268c3125c49518127cf33a6ade9b0d7c8de6309b
Samuel Factor <sfxfactor@gmail.com>
2015-09-24 18:39:02 -0500

    added stub function for finding center'

--- findStar.py
+++ findStar.py
@@ -7,8 +7,17 @@ files = ascii.read('NIRC2_sci_20020_1.txt')
 files = np.array(files['fileNames'])
 
 f = open('starPositions.txt','w')
-f.write('x\ty\n')
+f.write('file\tx\ty\n')
+xg, yg = 611, 471
+
+def findStar(im, (xg, yg)):
+    return (xg, yg)
 
 for i in files:
     im = fits.getdata('calfits/'+i[:-5]+'_drp.fits')
+    x, y = findStar(im, (xg, yg))
+    f.write(i[12:-5]+'\t'+str(x)+"\t"+str(y)+'\n')
+
+f.close()
+
 
