4d89d9655e6c73d422ef82857aef864645e148d2
Samuel Factor <sfxfactor@gmail.com>
2015-09-24 18:30:54 -0500

    streamlined the fits import

--- findStar.py
+++ findStar.py
@@ -4,9 +4,11 @@ import numpy as np
 
 
 files = ascii.read('NIRC2_sci_20020_1.txt')
+files = np.array(files['fileNames'])
+
 f = open('starPositions.txt','w')
 f.write('x\ty\n')
 
-for i in files['fileNames']:
+for i in files:
     im = fits.getdata('calfits/'+i[:-5]+'_drp.fits')
 
