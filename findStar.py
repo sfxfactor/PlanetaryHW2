562b5032f4f8383b583930a4a97ee300ae291b01
Samuel Factor <sfxfactor@gmail.com>
2015-09-24 19:39:41 -0500

    finished findstar. computed flux weighted centroid inside 5 pixel radius of a guess

--- findStar.py
+++ findStar.py
@@ -8,16 +8,25 @@ files = np.array(files['fileNames'])
 
 f = open('starPositions.txt','w')
 f.write('file\tx\ty\n')
-xg, yg = 611, 471
 
 def findStar(im, (xg, yg)):
-    return (xg, yg)
+    x, y, tf = 0, 0, 0
+    for i in range(xg-5,xg+6):
+        for j in range(yg-5,yg+6):
+            if (np.sqrt((i-xg)**2+(j-yg)**2)<=5):
+                f = im[i,j]
+                tf += f
+                x += f*i
+                y += f*j
 
+    x = x/tf
+    y = y/tf
+    return (x, y)
+
+xg, yg = 611, 471
 for i in files:
     im = fits.getdata('calfits/'+i[:-5]+'_drp.fits')
     x, y = findStar(im, (xg, yg))
     f.write(i[12:-5]+'\t'+str(x)+"\t"+str(y)+'\n')
 
 f.close()
-
-
