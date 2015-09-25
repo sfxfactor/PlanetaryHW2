f5a8a66c5547edda42c3c3fcfbfe775a80904b55
Samuel Factor <sfxfactor@gmail.com>
2015-09-25 17:33:32 -0500

    finished program to subtract averaged rings from data. rm *.pycrm *.pycrm *.pycrm *.pycrm *.pycrm *.pyc! need to go through code and make sure x and y are correct- they are switched in imagesrm *.pycrm *.pycrm *.pycrm *.pycrm *.pyc

---
+++ averings.py
@@ -0,0 +1,49 @@
+import numpy as np
+import scipy.interpolate as intp
+from astropy.io import fits
+from astropy.io import ascii
+import sys
+
+files = ascii.read('NIRC2_sci_20020_1.txt')
+fileNames = np.array(files['fileNames'])
+targets = np.array(files['target'])
+
+positions = ascii.read('starPositions.txt')
+
+ROXs42B = np.array([])
+ROXs12 = np.array([])
+n = np.size(targets)
+for i in range(n):
+    
+    im = np.transpose(fits.getdata('calfits/'+fileNames[i]))
+    x, y = positions['x'][i], positions['y'][i]
+
+    smooth = intp.RectBivariateSpline(range(1024),range(1024),im)
+
+    R = np.arange(300)
+    theta = np.arange(360)
+    f = np.zeros((300,360))
+
+    for r in R:
+        for t in theta:
+            trad = np.radians(t)
+            xp = x + r*np.cos(trad)
+            yp = y + r*np.sin(trad)
+            f[r,t] = smooth(xp,yp)
+
+    f = np.median(f, axis=1)
+    
+    smoothf = intp.interp1d(R,f,bounds_error=False,fill_value=0.0)
+    xp, yp = np.arange(1024),np.arange(1024)
+    xg, yg = np.meshgrid(xp,yp)
+    rp = np.sqrt((xg-x)**2 + (yg-y)**2)
+
+    imsub = im - smoothf(np.transpose(rp))
+
+    fits.writeto('calfits/'+fileNames[i][:-5]+'.ringsub.fits',np.transpose(imsub))
+
+    percent = float(i) / n
+    hashes = '#' * int(round(percent * 20))
+    spaces = ' ' * (20 - len(hashes))
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
