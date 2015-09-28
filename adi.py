84f17756e124bbcd927c9c9b0e24b701db585178
Samuel Factor <sfactor@wesleyan.edu>
2015-09-28 13:42:36 -0500

    added script to execute all code. finished adi. fixed small bugs in adi and psf.

--- adi.py
+++ adi.py
@@ -13,16 +13,31 @@ ROXs12 = fits.getdata('results/ROXs12med.fits')
 o = 512 #psf center (origin)
 
 n = np.size(targets)
+
+def findRatio(im,psf,(xc,yc),r,d):
+    xs, ys = np.shape(im)
+    ratios = np.array([])
+    for i in range(xs):
+        for j in range(ys):
+            if (np.sqrt((i-xc)**2+(j-yc)**2)<r+d and np.sqrt((i-xc)**2+(j-yc)**2)>r):
+                ratios = np.append(ratios,im[j,i]/psf[j,i])
+    return np.median(ratios)
+
 for i in range(n):
     if targets[i]!=0:
-        im = fits.getdata('/'+fileNames[i])[:-5]+'.reg.fits')
+        im = fits.getdata('results/'+fileNames[i][:-5]+'.reg.fits')
 
         if targets[i]==1:
             psf = ROXs42B
-            #find scaling ratio
         if targets[i]==2:
             psf = ROXs12
-            #find scaling ratio
-
+        s = findRatio(im,psf,(o,o),15,15)
         im = im - s*psf
         fits.writeto('results/'+fileNames[i][:-5]+'.adi.fits',im)
+
+
+    percent = float(i) / n
+    hashes = '#' * int(round(percent * 20))
+    spaces = ' ' * (20 - len(hashes))
+    sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
---
+++ exeCode.sh
@@ -0,0 +1,4 @@
+python findStar.py
+python psf.py
+python averings.py
+python adi.py
